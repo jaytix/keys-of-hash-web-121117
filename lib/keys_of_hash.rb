5a250d6332231634cac3028ef2531485e497d23b
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-28 19:22:55 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,12 @@
 require 'pry'
 
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     array = []
-    
+    self.each do |animal|
+      
     binding.pry
     return array
+    end
   end
 end
