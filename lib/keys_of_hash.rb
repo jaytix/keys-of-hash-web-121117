f9014658d173b3bec1fca29e8f247fb7c2e242c7
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-28 19:26:24 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,9 +4,13 @@ class Hash
   def keys_of(*arguments)
     array = []
     self.each do |animal|
-
+      animal.each do |stats|
+        stats.each do |string|
+          if string == *arguments
+            array.push(string)
     binding.pry
+        end
+      end
     return array
-    end
   end
 end
